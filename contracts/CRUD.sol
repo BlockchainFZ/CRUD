52bebbd084d14ad9a552564b0310a2886004906d
Admin <admin@Admins-MacBook-Pro-2.local>
2019-12-16 14:25:48 +0000

    refactor update functions

--- contracts/CRUD.sol
+++ contracts/CRUD.sol
@@ -1,51 +1,74 @@
 pragma solidity >=0.4.0 <0.7.0;
 
-    contract messageTest {
+    contract UserCrud {
 
-    struct userStruct {
+      struct UserStruct {
         bytes32 userEmail;
         uint userAge;
         uint index;
+      }
+
+      mapping(address => UserStruct) private userStructs;
+      address[] private userIndex;
+
+      event LogNewUser(address indexed userAddress, uint index, bytes32 userEmail, uint userAge);
+      event LogUpdateUser(address indexed userAddress, uint index, bytes32 userEmail, uint userAge);
+
+      function isUser(address userAddress) public view returns(bool isIndeed) {
+        if(userIndex.length == 0) return false;
+        return (userIndex[userStructs[userAddress].index] == userAddress);
+      }
+
+      function insertUser(address userAddress, bytes32 userEmail, uint userAge) public returns(uint index) {
+        if(isUser(userAddress)) throw;
+        userStructs[userAddress].userEmail = userEmail;
+        userStructs[userAddress].userAge = userAge;
+        userStructs[userAddress].index = userIndex.push(userAddress) - 1;
+          LogNewUser(
+            userAddress,
+            userStructs[userAddress].index,
+            userEmail,
+            userAge);
+            )
+          return userIndex.length - 1;
+      }
+
+      function getUser(address userAddress) public view returns(bytes32 userEmail, uint userAge, uint index) {
+        if(!isUser(userAddress)) throw;
+        return(
+        userStructs[userAddress].userEmail,
+        userStructs[userAddress].userAge,
+        userStructs[userAddress].index);
+
+      }
+
+      function updateUserEmail(address userAddress, bytes32 userEmail) public returns(bool success) {
+        if(!isUser(userAddress)) throw;
+        userStructs[userAddress].userEmail = userEmail;
+        LogUpdateUser(
+          userAddress,
+          userStructs[userAddress].index,
+          userEmail,
+          userStructs[userAddress].userAge);
+          return true;
+      }
+
+      function updateUserAge(address userAddress, uint userAge) public returns(bool success) {
+        if(!isUser(userAddress)) throw;
+        userStructs[userAddress].userAge = userAge;
+        LogUpdateUser(
+          userAddress,
+          userStructs[userAddress].index = index;
+          userStructs[userAddress].email = email;
+          userAge);
+          return true;
+      }
+
+      function getUserCount() public view returns(uint count) {
+        return userIndex.length;
+      }
+
+      function getUserAtIndex(uint index) public view returns (address userAddress) {
+        return(userIndex[index]);
+      }
     }
-
-    mapping (address => userStruct) private userStructs;
-    address[] private userIndex;
-
-    function insertUser(
-        address _userAddress,
-        bytes32 _userEmail,
-        uint _userAge) public returns(bool success) {
-
-        userStructs[_userAddress].userEmail = _userEmail;
-        userStructs[_userAddress].userAge = _userAge;
-        userStructs[_userAddress].index = userIndex.push(_userAddress) -1;
-        return true;
-    }
-
-    function getUser
-    (address _userAddress) public view
-    returns(bytes32 userEmail, uint userAge) {
-        userStructs[_userAddress].userEmail;
-        userStructs[_userAddress].userAge;
-    }
-
-
-    function updateUser(address _userAddress, bytes32 _userEmail,uint _userAge)
-    public returns (bool success) {
-        userStructs[_userAddress].userEmail = _userEmail;
-        userStructs[_userAddress].userAge = _userAge;
-
-    }
-
-
-
-
-
-
-}
-
-
-
-
-
-    
