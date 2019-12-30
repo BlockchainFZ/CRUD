78a5f3019c74dcc09bc21d985b1080a1448027d9
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2019-12-30 17:49:32 +0000

    Allows a users email address to be updated

--- test/test.js
+++ test/test.js
@@ -7,6 +7,7 @@ const truffleAssert = require('truffle-assertions');
 
     let contract;
     let owner = accounts[0];
+    let secondAddress = accounts[1];
 
     beforeEach(async() => {
       contract = await MigrationContract.new({from:owner});
@@ -20,18 +21,33 @@ const truffleAssert = require('truffle-assertions');
     it('Allows a user to be added', async() => {
       let userEmailHex = web3.utils.asciiToHex("kevinkeaveney@hotmail.com");
       let userEmailAsc = web3.utils.hexToAscii(userEmailHex);
-      let userEmailAsc32 = web3.utils.padLeft(userEmailHex, 48);
+
       await contract.insertUser(owner, userEmailHex, 42);
       let user = await contract.getUser(owner);
       let userEmail = user.userEmail;
       userEmailAsc32 = web3.utils.padRight(userEmail, 16);
-      console.log(userEmailHex);
-      console.log(userEmail);
-      console.log(userEmailAsc32);
+
       assert.equal(user.userAge.toNumber(), 42, "Incorrect user age");
-      //assert.equal(user.userEmail,userEmailHex, "Incorrect user email");
+      assert.equal(user.userEmail,userEmailAsc32, "Incorrect user email");
+
+      let userAddress = await contract.getUserAtIndex(0);
+      assert.equal(userAddress, owner, "Incorrect User Address");
+
+      });
+
+      it('Allows a users email address to be updated', async() => {
+        let userEmailHex = web3.utils.asciiToHex("kevinkeaveney@hotmail.com");
+        let newUserEmailHex = web3.utils.asciiToHex("kevinkeaveney@gmail.com");
+        await contract.insertUser(owner, userEmailHex, 42);
+        await contract.updateUserEmail(owner, newUserEmailHex);
+        let user = await contract.getUser(owner);
+        let userOldEmail = web3.utils.toAscii(userEmailHex);
+        let userNewEmail = web3.utils.toAscii(user.userEmail);
+        //console.log('Old user email address', userOldEmail);
+        //console.log('New user email address', userNewEmail);
+        assert.notEqual(userOldEmail, userNewEmail, "Email address has not changed");
 
 
-    })
+      });
 
   });
