5d62738be6c676604ccaff70ba3099a31a7dc366
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2019-12-30 19:04:04 +0000

    Emmits LogNewUser event

--- test/truffleEventTest.js
+++ test/truffleEventTest.js
@@ -6,10 +6,23 @@ const truffleAssert = require('truffle-assertions');
     let contract;
     let owner = accounts[0];
     let account2 = accounts[1];
+    let userEmailHex = web3.utils.asciiToHex("kevinkeaveney@hotmail.com");
 
 
     beforeEach(async() => {
       contract = await MigrationContract.new({ from:owner});
+
+    });
+
+    it('Emmits LogNewUser event when insertUser is called ', async() => {
+      let tx = await contract.insertUser(owner, userEmailHex, 42);
+      truffleAssert.eventEmitted(tx,'LogNewUser',(event) => {
+        return(event.userAddress == owner && event.userAge == 42);
+      });
+    });
+
+
+    it('Emmits LogUpdateUser event when updateUserEmail is called', async() => {
 
     });
 
